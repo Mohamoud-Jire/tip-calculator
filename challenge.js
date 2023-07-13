fa89291014b298b619beac155785388793a1872d
Mohamoud-Jire <amoh54932@gmail.com>
2023-07-13 17:56:07 +0300

    Answered

--- challenge.js
+++ challenge.js
@@ -1,2 +1,5 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let bill = 229;
+let tip = 34.35;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`)
